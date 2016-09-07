f3b7b1a90f354231127915e36ba596342af69756
ccsorip <coni.csorip@gmail.com>
2016-09-07 18:12:06 -0300

    Initial files to assign sucursal to ficha

---
+++ Assignment/Assignment/ContratoAsucursal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace Assignment
+{
+    class ContratoAsucursal : CodeActivity
+    {
+        [Input("Supervisor")]
+        [ReferenceTarget("systemuser")]
+        public InArgument<EntityReference> user { get; set; }
+
+        
+    }
+}
---
+++ Assignment/Assignment/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace Assignment
+{
+    class Utils
+    {
+
+        public static bool addOrUpdateParameter(ref Entity e, string parameterName, Object o)
+        {
+            if (e.Contains(parameterName))
+            {
+                bool resp = e[parameterName] != o;
+                e[parameterName] = o;
+                return resp;
+            }
+            else
+            {
+                e.Attributes.Add(parameterName, o);
+                return true;
+            }
+        }
+    }
+}
